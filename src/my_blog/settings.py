2e2fec47cfe149c904f822503272a4b2fd90de0d
hamdyadam97 <hamdyadam97@gmail.com>
2019-07-15 22:36:15 +0200

    static for all project

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    'C:/Users/hamdy/Blogger/src/static'
+
+]
